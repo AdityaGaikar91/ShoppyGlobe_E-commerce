f0b30e56bdfe592207f78b7f2383571460c24f9c
AdityaGaikar91 <adityagaikar91@gmail.com>
2025-11-23 00:19:42 +0530

    Add JSDoc comments to store configuration

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './cartSlice'
 
+/**
+ * Redux Store Configuration
+ * Combines all reducers and configures the store
+ */
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
